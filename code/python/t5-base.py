dc08178009c6c6b1c04760e109c0a8466a6d7398
Bhagyashreet20 <btaleka2@illinois.edu>
2024-03-29 11:40:55 -0500

    commiting the code with slurm script and few logs for bringing up cpu version of t5 base

---
+++ code/python/t5-base.py
@@ -0,0 +1,19 @@
+
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+from transformers import file_utils
+
+print("******downloading tokenizer******")
+tokenizer = T5Tokenizer.from_pretrained("google/flan-t5-base")
+print("************done with downloading tokenizer*************")
+print('***********downloading base t5 model******************')
+model = T5ForConditionalGeneration.from_pretrained("google/flan-t5-base")
+print("******************downloaded base t5 model***********************")
+
+cache_dir = file_utils.default_cache_path
+print(f"Current Transformers cache directory: {cache_dir}")
+
+input_text = "translate English to German: How old are you?"
+input_ids = tokenizer(input_text, return_tensors="pt").input_ids
+
+outputs = model.generate(input_ids)
+print(tokenizer.decode(outputs[0]))
